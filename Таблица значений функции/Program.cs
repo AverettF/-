a35e01ee45ad5a5bd289604ee56d5cda22dc7f23
FedorShuvalov <shuvalov.fedor18@outlook.com>
2020-10-10 11:31:01 +0500

    ТОЧНО рабочая версия,но только с целыми числами
    Не сделано:
    1)Проверка на дурака
    2)Варинат с типом double
    3)Повторения кода(здесь хуже всего.Пока не ясно как оптимизировать,абсолютно одинаковый код в методах для ХУ и Fx,Fy, но разные типы вывода)
    4)Написать метод для оглавления

--- Таблица значений функции/Program.cs
+++ Таблица значений функции/Program.cs
@@ -4,7 +4,7 @@ namespace Таблица_значений_функции
 {
     class Program
     {
-        static int Function(int x)
+        static int GetFunctionY(int x)
         {
             return 10*x;//x * x - 4 * x - 2;
         }
@@ -14,7 +14,7 @@ namespace Таблица_значений_функции
             int maxLength = 0;
             for (int i = forRange; i <= toRange; i += step)
             {
-                int intFuncL = Convert.ToString(Function(i)).Length;
+                int intFuncL = Convert.ToString(GetFunctionY(i)).Length;
                 if (intFuncL > maxLength)
                     maxLength = intFuncL;
             }
@@ -26,39 +26,77 @@ namespace Таблица_значений_функции
             return Convert.ToInt32(Console.ReadLine());
         }
 
-        static void PrintBox(int maxLength,int step, int forRange, int toRange, int length)
+        static void PrintBox(int maxLength,int step, int forRange, int toRange)
         {
             Console.WriteLine(maxLength);
             PrintLine(maxLength, "╔", "═", "╗");//╔═══════╗
-                                                //║       ║
+                                                //║ Оглав ║
             PrintLine(maxLength, "╠", "╦", "╣");//╠═══╦═══╣
-                                                //║ X ║ Y ║
+            PrintXandYline(maxLength);          //║ X ║ Y ║
             PrintLine(maxLength, "╠", "╬", "╣");//╠═══╬═══╣
-                                                //║...║...║
+            PrintNumberLine(step, forRange, toRange, maxLength);              //║...║...║
             PrintLine(maxLength, "╚", "╩", "╝");//╚═══╩═══╝
         }
 
         static void PrintLine(int maxLength, string lineLeft, string lineMiddle,  string lineRight)
         {
             Console.Write(lineLeft);
-            for (int i = 0; i <= maxLength; i++)
+            for (int i = 1; i <= maxLength; i++)
                 Console.Write("═");
             Console.Write(lineMiddle);
-            for (int i = 0; i <= maxLength; i++)
+            for (int i = 1; i <= maxLength; i++)
                 Console.Write("═");
             Console.WriteLine(lineRight);
         }
 
         static void PrintNumberLine(int step, int forRange, int toRange, int length)
         {
-            for (int x = forRange; x <= toRange; x += length)
+            for (int x = forRange; x <= toRange; x += step)
             {
-                int lengthX = Convert.ToString(x).Length;
-                int y = Function(x);
-                int lengthY = Convert.ToString(y).Length;
+                int numberLengthX = Convert.ToString(x).Length;
+                int numberLengthY = Convert.ToString(GetFunctionY(x)).Length;
 
+                Console.Write("║");
+                for (int i = 1; i <= (length - numberLengthX) / 2; i++)
+                    Console.Write(" ");
+                Console.Write(x);
+                for (int i = 1; i <= (length - numberLengthX) / 2; i++)
+                    Console.Write(" ");
+                if ((length - numberLengthX) % 2 == 1)
+                    Console.Write(" ");
+                                Console.Write("║");
 
+                for (int i = 1; i <= (length - numberLengthY) / 2; i++)
+                    Console.Write(" ");
+                Console.Write(GetFunctionY(x));
+                for (int i = 1; i <= (length - numberLengthY) / 2; i++)
+                    Console.Write(" ");
+                if ((length - numberLengthY) % 2 == 1)
+                    Console.Write(" ");
+                Console.WriteLine("║");
             }
+        }
+
+        static void PrintXandYline( int length)
+        {
+            Console.Write("║");
+            for (int i = 1; i <= (length - 1) / 2; i++)
+                Console.Write(" ");
+            Console.Write("x");
+            for (int i = 1; i <= (length - 1) / 2; i++)
+                Console.Write(" ");
+            Console.Write("║");
+            if ((length - 1) % 2 == 1)
+                Console.Write(" ");
+
+            for (int i = 1; i <= (length - 1) / 2; i++)
+                Console.Write(" ");
+            Console.Write("y");
+            for (int i = 1; i <= (length - 1) / 2; i++)
+                Console.Write(" ");
+            if ((length - 1) % 2 == 1)
+                Console.Write(" ");
+            Console.WriteLine("║");
         }
 
         static void Main(string[] args)
@@ -77,7 +115,7 @@ namespace Таблица_значений_функции
 
 
             int maxLength = FindMaxLength(step, forRange, toRange);
-            PrintBox(maxLength, step, forRange, toRange, maxLength);
+            PrintBox(maxLength, step, forRange, toRange);
 
         }
     }
